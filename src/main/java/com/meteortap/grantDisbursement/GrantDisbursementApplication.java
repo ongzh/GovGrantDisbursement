aa231c47526a3b47f1804b3383b0435f45019922
Ong <ong2323@gmail.com>
2022-09-10 19:16:45 -0400

    added initial uml diagram

---
+++ src/main/java/com/meteortap/grantDisbursement/GrantDisbursementApplication.java
@@ -0,0 +1,13 @@
+package com.meteortap.grantDisbursement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrantDisbursementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrantDisbursementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meteortap/grantDisbursement/GrantDisbursementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteortap.grantDisbursement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrantDisbursementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
